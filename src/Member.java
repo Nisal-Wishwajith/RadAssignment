250673711ba70d4a6cf73ba0548279bf8b75629e
THE NISAL <2021is073@stu.ucsc.cmb.ac.lk>
2023-09-28 02:58:28 +0530

    Member completed

--- src/Member.java
+++ src/Member.java
@@ -1,5 +1,43 @@
+import java.util.*;
+
 public class Member {
-    private String MemberID ;
-    private String MemberName ;
-    private int booksBorrowed;
+    private String memberId ;
+    private String name ;
+    private List<Book> booksBorrowed ;
+
+    public Member(String MemberID , String MemberName) {
+        this.memberId=MemberID;
+        this.name=MemberName;
+    }
+
+
+// getter and setter for MemberID
+    public String getMemberId() {
+        return this.memberId;
+    }
+    
+    public void setMemberId(String NewMemberId){
+        this.memberId=NewMemberId;
+    }
+
+
+// getter and setter for Member's Name
+        public String getName() {
+        return this.name;
+    }
+
+    public void setName(String newName){
+        this.name=newName;
+    }
+
+
+// getter and setter for bookBorrowed
+            public List<Book> getbookBorrowed() {
+        return this.booksBorrowed;
+    }
+
+    public void setbookBorrowed(List<Book> newbookBorrowed){
+        this.booksBorrowed=newbookBorrowed;
+    }
 }
+
