eb7ea7bcdc3883092fbc501fa5df52ae756a19f1
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2023-09-28 04:09:02 +0530

    member

--- src/Member.java
+++ src/Member.java
@@ -1,43 +1,39 @@
 import java.util.*;
 
 public class Member {
-    private String memberId ;
-    private String name ;
-    private List<Book> booksBorrowed ;
+    private String memberId;
+    private String name;
+    private List<Book> booksBorrowed;
 
-    public Member(String MemberID , String MemberName) {
-        this.memberId=MemberID;
-        this.name=MemberName;
+    public Member(String MemberID, String MemberName) {
+        this.memberId = MemberID;
+        this.name = MemberName;
     }
 
-
-// getter and setter for MemberID
+    // getter and setter for MemberID
     public String getMemberId() {
         return this.memberId;
     }
-    
-    public void setMemberId(String NewMemberId){
-        this.memberId=NewMemberId;
+
+    public void setMemberId(String NewMemberId) {
+        this.memberId = NewMemberId;
     }
 
-
-// getter and setter for Member's Name
-        public String getName() {
+    // getter and setter for Member's Name
+    public String getName() {
         return this.name;
     }
 
-    public void setName(String newName){
-        this.name=newName;
+    public void setName(String newName) {
+        this.name = newName;
     }
 
-
-// getter and setter for bookBorrowed
-            public List<Book> getbookBorrowed() {
+    // getter and setter for bookBorrowed
+    public List<Book> getbookBorrowed() {
         return this.booksBorrowed;
     }
 
-    public void setbookBorrowed(List<Book> newbookBorrowed){
-        this.booksBorrowed=newbookBorrowed;
+    public void setbookBorrowed(List<Book> newbookBorrowed) {
+        this.booksBorrowed = newbookBorrowed;
     }
 }
-
